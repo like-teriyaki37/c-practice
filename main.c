bdb731672537f18f9245bc1ee93509e4889ff5a9
like-teriyaki37 <anton.zxwxc@gmail.com>
2025-12-21 09:27:17 +0200

    make an http request using curl

--- main.c
+++ main.c
@@ -1,1 +1,16 @@
-// test
+#include <stdio.h>
+#include <curl/curl.h>
+
+int main() {
+    CURL *curl;
+    CURLcode res;
+
+    curl = curl_easy_init();
+    if(curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, "https://httpbin.org/get");
+        res = curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+    }
+    printf("Hello, World!\n");
+    return 0;
+}
